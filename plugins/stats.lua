8fd01d8b7eba265fab3ea50ff8d97e446db069f8
elt11bot <aryaneskandari1381@gmail.com>
2016-03-19 03:00:24 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (elt)",-- Put everything you like :)
+		"^[!/]([Ee]lt)"-- Put everything you like :)
     }, 
   run = run
 }
